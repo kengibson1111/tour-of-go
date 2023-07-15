f7972e217f194fbe6b9028daa7873e51220ca3fe
Ken Gibson <kengibson1111@gmail.com>
2023-07-15 05:15:55 +0000

    Workspace and module changes

---
+++ generics/tourofgo-generics-typeparams/README.md
@@ -0,0 +1,3 @@
+# generics (type parameters)
+
+Type parameters are the core of Go generic functions. Strong typing is very effective, and without generic functions strong typing means writing a lot of redundant functions only because of the strong typing. Type parameters allow us to write a generic function that could apply to multiple types.
--- generics/tourofgo-generics-typeparams/tourofgo-generics-typeparams.go
+++ generics/tourofgo-generics-typeparams/tourofgo-generics-typeparams.go
@@ -2,18 +2,24 @@ package main
 
 import "fmt"
 
-type Person struct {
-	Name string
-	Age  int
-}
-
-func (p Person) String() string {
-	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
+// Index returns the index of x in s, or -1 if not found.
+func Index[T comparable](s []T, x T) int {
+	for i, v := range s {
+		// v and x are type T, which has the comparable
+		// constraint, so we can use == here.
+		if v == x {
+			return i
+		}
+	}
+	return -1
 }
 
 func main() {
-	a := Person{"Arthur Dent", 42}
-	z := Person{"Zaphod Beeblebrox", 9001}
-	fmt.Println(a, z)
-}
+	// Index works on a slice of ints
+	si := []int{10, 20, 15, -10}
+	fmt.Println(Index(si, 15))
 
+	// Index also works on a slice of strings
+	ss := []string{"foo", "bar", "baz"}
+	fmt.Println(Index(ss, "hello"))
+}
